e099e3006567d64a0b3a5dd7fca1b011f53e8c3a
AZLee <amjedzehani@gmai.com>
2024-08-20 18:00:34 +0200

    creation de lapp et des different dossier et installation des dep

--- Back/app.js
+++ Back/app.js
@@ -1,23 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
+const path = require('path');
+const password = require('./utils/password')
 
+// Création de l'application
 const app = express();
+// Middleware permettant à Express d'extraire le corps JSON des requêtes POST
+app.use(express.json());
 
+// Middleware gérant les erreurs de CORS
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    // Accès à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Autorisation d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Autorisation d'envoyer des requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
- });
-  
-app.use((req, res, next) => {
-    res.status(201);
-    next();
- });
+});
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+// Gestion de la ressource images de manière statique
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
- });
+// Enregistrement des routeurs
+app.use('/api/auth', userRoutes);
+app.use('/api/books', booksRoutes);
 
 module.exports = app;
